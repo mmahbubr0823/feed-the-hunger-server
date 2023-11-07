d38d964ff265014cb2d9beadefc207a462751925
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-11-08 02:23:00 +0600

    delete operation made for requested food

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
           });
 
 
+          app.delete('/requested-foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodsRequestCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
